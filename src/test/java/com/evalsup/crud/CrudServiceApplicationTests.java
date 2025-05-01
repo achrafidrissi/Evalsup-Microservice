8d51a067014daa83ca6c17e4f8b14cf8f25945a7
achrafidrissi <elaidiidrissiachraf@gmail.com>
2025-05-01 15:31:45 +0100

    First commit

---
+++ src/test/java/com/evalsup/crud/CrudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evalsup.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
